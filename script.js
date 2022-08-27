31b0ea67efbd4c05c0c9cba280fa3b5671934488
brandonslimwilliams <brandonslimwilliams@gmail.com>
2022-08-26 19:41:35 -0500

    created loop and conditional fot timeNow and timeblock

--- script.js
+++ script.js
@@ -1,25 +1,42 @@
-var date = moment().format("MMM Do YY"); 
-$('#currentDay').html(date);
+var date = moment().format("MMM Do YY");
+$("#currentDay").html(date);
 
-$(document).ready(function(){
-    //click listener for button 
-$(".saveB").on("click", function(event) {
-    //define text and time variables 
+$(document).ready(function () {
+  //click listener for button
+  $(".saveB").on("click", function (event) {
+    //define text and time variables
     var text = $(this).siblings("planning").val();
-    var time = $(this).parent().attr('id')
+    var time = $(this).parent().attr("id");
 
     //items to be saved to localStorage
-    localStorage.setItem(text, time)
+    localStorage.setItem(text, time);
 
-})
+});
 
 function currentTime() {
-    //variable for the time now with moment.js
-    var timeNow = moment().hours();
+  //variable for the time now with moment.js
+  var timeNow = moment().hours();
 
-    //loop over the timeblocks and compare it with the timeNow variable 
-    
+  //loop over the timeblocks and compare it with the timeNow variable
+  $(".time-block").each(function () {
+    var blockTime = parseInt($(this).attr("id").split("hour")[1]);
 
+    //check current time and add appropriate classes
+    if (blockTime < timeNow) {
+      $(this).addClass("past");
+    } else if (blockTime === timeNow) {
+        $(this).removeClass('past');
+        $(this).addClass('present');
+        
+    }else {
+        $(this).removeClass('past');
+        $(this).removeClass('present');
+        $(this).addClass('future');
+    }
+  });
 }
 
+//get Items from localStorage 
+
+currentTime();
 })
